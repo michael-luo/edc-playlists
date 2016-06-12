242c3f4e394ff5f2fb6d565e911cde13a84eaf06
Michael Luo <miczluo@gmail.com>
2016-06-12 14:08:53 -0700

    Added request and error logging

--- server/server.js
+++ server/server.js
@@ -52,6 +52,8 @@ import routes from '../shared/routes';
 import { fetchComponentData } from './util/fetchData';
 import apiRoutes from './routes/api.routes';
 import serverConfig from './config';
+import logger from 'winston';
+import expressLogger from 'express-winston';
 
 // Connect to MongoDB
 import connectToMongoDb from './connectToMongoDb';
@@ -74,7 +76,33 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(Express.static(path.resolve(__dirname, '../static')));
 
+// Log requests
+app.use(expressLogger.logger({
+  transports: [
+    new logger.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ],
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+  colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
 app.use('/api', apiRoutes(passport));
+
+// Log errors
+app.use(expressLogger.errorLogger({
+  transports: [
+    new logger.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
